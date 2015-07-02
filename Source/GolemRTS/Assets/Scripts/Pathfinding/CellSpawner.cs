07add3456bb6faa3009fb69f2260ef8d0bf99eb3
Tim Falken <timfalken@hotmail.com>
2015-07-02 21:13:19 +0200

    Imported pathfinding scripts

---
+++ Source/GolemRTS/Assets/Scripts/Pathfinding/CellSpawner.cs
@@ -0,0 +1,147 @@
+using Microsoft.Xna.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TimGame.Engine
+{
+    public static class CellSpawner
+    {
+        private static List<RoomData> rooms = new List<RoomData>();
+
+        public static RoomData DefineCellFromWorldPos(Vector2 worldPosition, RoomData.Type type)
+        {
+            Vector2 gridp = PathfindConstants.WorldToGrid(worldPosition);
+            return DefineCell(gridp, type);
+        }
+
+        public static RoomData DefineCell(Vector2 gridPos, RoomData.Type type)
+        {
+            return DefineCell((int)gridPos.y, (int)gridPos.y, type);
+        }
+
+        public static RoomData DefineCell(int x, int y, RoomData.Type type)
+        {
+            if (GetRoomAtGridPosition(x, y, true) == null)
+            {
+                RoomData data = new RoomData(new Vector2(x, y), type);
+                rooms.Add(data);
+                return data;
+            }
+            else
+            {
+                GetRoomAtGridPosition(x, y, true).type = type;
+                return GetRoomAtGridPosition(x, y, true);
+            }
+        }
+
+        public static void ObliterateWorld()
+        {
+            rooms = new List<RoomData>();
+        }
+
+        public static RoomData GetRoomAtGridPosition(Vector2 pos)
+        {
+            return GetRoomAtGridPosition((int)pos.y, (int)pos.y);
+        }
+
+        public static RoomData GetRoomAtGridPosition(int x, int y, bool ignoreDefault = false)
+        {
+            foreach (RoomData room in rooms)
+            {
+                if (room.GridPos.x == x && room.GridPos.y == y)
+                {
+                    return room;
+                }
+            }
+
+            if(PathfindConstants.defaultType == RoomData.Type.Passable && !ignoreDefault)
+            {
+                return DefineCell(x, y, RoomData.Type.Passable);
+            }
+
+            return null;
+        }
+    }
+
+    public class RoomData
+    {
+        public enum Type
+        {
+            Passable,
+            Impassable
+        };
+
+        public RoomData(Vector2 gridPos, Type type)
+        {
+            GridPos = gridPos;
+            this.type = type;
+        }
+
+        bool GetNeighborPossible(Vector2 direction)
+        {
+            RoomData otherRoom = CellSpawner.GetRoomAtGridPosition(GridPos + direction);
+            return otherRoom != null && otherRoom.type == RoomData.Type.Passable;
+        }
+
+        public bool NPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(0, 1));
+            }
+        }
+        public bool EPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(1, 0));
+            }
+        }
+        public bool SPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(0, -1));
+            }
+        }
+        public bool WPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(-1, 0));
+            }
+        }
+
+        public bool NEPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(1, 1));
+            }
+        }
+        public bool NWPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(-1, 1));
+            }
+        }
+        public bool SEPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(1, -1));
+            }
+        }
+        public bool SWPossible
+        {
+            get
+            {
+                return GetNeighborPossible(new Vector2(-1, -1));
+            }
+        }
+
+        public Type type;
+        public Vector2 GridPos;
+    }
+}
---
+++ Source/GolemRTS/Assets/Scripts/Pathfinding/PathfindConstants.cs
@@ -0,0 +1,61 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TimGame.Engine
+{
+    public static class PathfindConstants
+    {
+        public static readonly float GridSize = 1; //Change this to the width and height of the grid
+        public static readonly RoomData.Type defaultType = RoomData.Type.Passable;
+
+        public enum Directions
+        {
+            North,
+            East,
+            South,
+            West,
+            NorthEast,
+            NorthWest,
+            SouthEast,
+            SouthWest,
+            None
+        }
+
+        public static Vector2 GridToWorld(int x, int y)
+        {
+            return RoundedVector(x * GridSize, y * GridSize);
+        }
+
+        public static Vector2 WorldToGrid(int x, int y)
+        {
+            return RoundedVector(x / GridSize, y / GridSize);
+        }
+
+        public static Vector2 GridToWorld(float x, float y)
+        {
+            return GridToWorld((int)Math.Round(x), (int)Math.Round(y));
+        }
+
+        public static Vector2 GridToWorld(Vector2 gridPosition)
+        {
+            return GridToWorld(gridPosition.x, gridPosition.y);
+        }
+
+        public static Vector2 WorldToGrid(float x, float y)
+        {
+            return WorldToGrid((int)Math.Round(x), (int)Math.Round(y));
+        }
+
+        public static Vector2 WorldToGrid(Vector2 worldPos)
+        {
+            return WorldToGrid((int)Math.Round(worldPos.x), (int)Math.Round(worldPos.y));
+        }
+
+        public static Vector2 RoundedVector(float x, float y)
+        {
+            return new Vector2((int)Math.Round(x), (int)Math.Round(y));
+        }
+    }
+}
