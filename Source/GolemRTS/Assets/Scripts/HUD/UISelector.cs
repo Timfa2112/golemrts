fd0d01b13872927d440053e663436bb671820ec1
Jeroen <jeroenesseveld@gmail.com>
2015-07-05 16:35:32 +0200

    Created UISelector for unit selection and control

---
+++ Source/GolemRTS/Assets/Scripts/HUD/UISelector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISelector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //TODO test user input and creature movement
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if(hit.collider != null)
+            {
+                Debug.Log ("Target Position: " + hit.collider.gameObject.transform.position);
+            }
+        }
+	}
+}
